4a0ce3393feb62242f80e6f146828ee5c6a5798d
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-05-07 09:37:10 -0700

    first commit

---
+++ src/components/Timeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Photo from './Photo';
+
+export default class Timeline extends Component {
+    render(){
+        return (
+        <div className="fotos container">
+          <Photo/>
+          <Photo/>
+        </div>            
+        );
+    }
+}
