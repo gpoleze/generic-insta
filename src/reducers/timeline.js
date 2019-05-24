0ebcf0ab28eee94ff3a0bb129dda0eeb7fd5045c
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-05-23 22:12:01 -0700

    Refactores the reducers to use an immutable approach

--- src/reducers/timeline.js
+++ src/reducers/timeline.js
@@ -1,40 +1,45 @@
+import {List} from "immutable";
+import {Photo} from "../components/photo-box/Photo";
+
+
 export const ActionType = {
     LIST: "LIST",
     COMMENT: 'COMMENT',
     LIKE: 'LIKE'
 };
 
+const changePhoto = (photos, id, updateCallback) => {
+    const oldPhotoState = photos.find(photo => photo.id === id);
+
+    const newPhotosState = new Photo(oldPhotoState).update(updateCallback(oldPhotoState));
+    const listIndex = photos.findIndex(photo => photo.id === id);
+    return photos.set(listIndex, newPhotosState);
+};
+
 export const timeline = (state = [], action) => {
     if (action.type === ActionType.LIST) {
-        return action.photos;
+        return new List(action.photos);
     }
 
-    if (action.type === ActionType.COMMENT){
-
-        const photos = [].concat(state);
-
-        const targetedPhoto = photos.find(photo => photo.id === action.id);
-        targetedPhoto.comments.push(action.comment);
-
-        return photos
+    if (action.type === ActionType.COMMENT) {
+        return changePhoto(state, action.id, oldPhotoState => {
+            const newComments = oldPhotoState.comments.concat(action.comment);
+            return {comments: newComments};
+        });
     }
 
-    if (action.type === ActionType.LIKE){
-        const photos = [].concat(state);
-        const {id, liker} = action;
+    if (action.type === ActionType.LIKE) {
+        return changePhoto(state, action.id, oldPhotoState => {
+            const hasLikeByLoggedInUser = !oldPhotoState.hasLikeByLoggedInUser;
 
-        const targetedPhoto = photos.find(photo => photo.id === id);
+            const hasLiker = !!oldPhotoState.likers.find(photosLiker => photosLiker.userLogin === action.liker.userLogin);
 
-        const hasLiker = !!targetedPhoto.likers.find(photosLiker => photosLiker.userLogin === liker.userLogin);
+            const newLikers = hasLiker
+                ? oldPhotoState.likers.filter(photosLiker => photosLiker.userLogin !== action.liker.userLogin)
+                : oldPhotoState.likers.concat(action.liker);
 
-        if (hasLiker)
-            targetedPhoto.likers = targetedPhoto.likers.filter(photosLiker => photosLiker.userLogin !== liker.userLogin);
-        else
-            targetedPhoto.likers.push(liker);
-
-        targetedPhoto.hasLikeByLoggedInUser = !targetedPhoto.hasLikeByLoggedInUser;
-
-        return photos
+            return {hasLikeByLoggedInUser: hasLikeByLoggedInUser, likers: newLikers};
+        });
     }
     return state;
 };
--- src/services/pubsub-channels.js
+++
@@ -1,5 +0,0 @@
-export const PubSubChannel = {
-    LIKES_UPDATES: 'likes-updates',
-    NEW_COMMENT_UPDATES: 'new-comment-updates',
-    TIMELINE: 'timeline'
-};
